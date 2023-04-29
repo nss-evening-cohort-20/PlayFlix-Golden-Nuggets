9335a87c2727b7ffc773244f16a781a9307d76a8
Stephen Rudge <sbrudge@comcast.net>
2023-04-29 09:54:55 -0500

    updates to CSS and added google button

--- Models/Games.cs
+++ Models/Games.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public int? Rating { get; set; }
+            public int? Rating { get; set; }
         public int? UserRating { get; set; }
         public string Genre { get; set; }
         public string GameImg { get; set;}
--- PlayFlix-firebase-FrontEnd-main/src/components/views/ApplicationViews.js
+++ PlayFlix-firebase-FrontEnd-main/src/components/views/ApplicationViews.js
@@ -11,13 +11,13 @@ export const ApplicationViews = () => {
 
   return (
     <>
-      <h1>A Blank Page!!</h1>
+      <h1>Test</h1>
       {/* logout button */}
       <button type="submit" onClick={onLogout}>
         Logout
       </button>
-      {/* move this component to where you want your PhotoUpload */}
-      
     </>
+  
+    
   );
 };
