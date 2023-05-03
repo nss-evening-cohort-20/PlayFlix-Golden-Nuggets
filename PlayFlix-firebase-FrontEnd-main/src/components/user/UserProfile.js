310b9a62dadcbf779057e6cffbee41db698884ea
Kyle Malone <Kylejaxonmalone93@gmail.com>
2023-05-02 20:05:53 -0500

    commiting c#

--- PlayFlix-firebase-FrontEnd-main/src/components/user/UserProfile.js
+++ PlayFlix-firebase-FrontEnd-main/src/components/user/UserProfile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./UserProfile.css"
 // import "./EditProfile";
 
 export const UserProfile = () => {
