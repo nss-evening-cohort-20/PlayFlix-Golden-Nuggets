e3a21de917a05eff0061b845b15c2eabb21f0514
Holden D Reed <holdendreed@hotmail.com>
2023-05-06 12:26:38 -0500

    Fixed scrolling kinda

--- PlayFlix-firebase-FrontEnd-main/src/components/games/Game.js
+++ PlayFlix-firebase-FrontEnd-main/src/components/games/Game.js
@@ -2,27 +2,9 @@ import { useEffect, useState } from "react";
 import "./Game.css"
 import { Link } from "react-router-dom";
 export const Game = ({id, title, img, description, rating, userRating, genre, iFrame}) => {
-return <div className="container">
-    <div>
-    {title}
-    </div>
-        <Link to={`/Games/play/${id}`}>
-        <a class="item"><img src={img} alt="Animals"></img></a>
+return <>
+        <Link to={`/Games/play/${id}`} class="item">
+        <img src={img}></img>
         </Link>
-    <div>
-    {description}
-    </div>
-    <div>
-    {rating}
-    </div>
-    <div>
-    {userRating}
-    </div>
-    <div>
-    {genre}
-    </div>  <div>
-    {title}
-    </div>
-</div>
-
+</>
 }
