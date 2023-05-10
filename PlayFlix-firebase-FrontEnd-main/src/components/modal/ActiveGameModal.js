046184519e9a83d94099b29d9597a8f8e4b00825
alvinwendt <alvinwendt@gmail.com>
2023-05-10 15:00:27 -0500

    Added popout gaming window

--- PlayFlix-firebase-FrontEnd-main/src/components/modal/ActiveGameModal.js
+++ PlayFlix-firebase-FrontEnd-main/src/components/modal/ActiveGameModal.js
@@ -6,6 +6,19 @@ import "./PopupModal.css";
 export const ActiveGameModal = ({ game, closeModal }) => {
   const [openModal, setOpenModal] = useState(false);
 
+  function Item({ url, title }) {
+    function handleClick() {
+      window.open(url, "_blank");
+    }
+
+    return (
+      <div>
+        <h2>{title}</h2>
+        <button onClick={handleClick}>Open in new tab</button>
+      </div>
+    );
+  }
+
   return (
     <>
       <Modal
@@ -19,18 +32,22 @@ export const ActiveGameModal = ({ game, closeModal }) => {
             <h2 id="modal-title" className="popup-modal-title">
               {game.title}
             </h2>
-            <img src={game.img} alt={game.title} />
+            <img src={game.gameImg} alt={game.title} />
             <p id="modal-description" className="popup-modal-description">
-              {game.description}
+              Description: {game.description}
             </p>
             <p id="modal-description" className="popup-modal-description">
-              {game.genre}
+              Genre: {game.genre}
             </p>
             <p id="modal-description" className="popup-modal-description">
-              {game.rating}
+              Rating: {game.rating}
             </p>
 
-            <Link to={`/Games/play/${game.id}`} className="item">
+            <Link
+              to={`/Games/play/${game.id}`}
+              target="_blank"
+              className="item"
+            >
               <Button variant="contained" color="primary">
                 Play Game
               </Button>
