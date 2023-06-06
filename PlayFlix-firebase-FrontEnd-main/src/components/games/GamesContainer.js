80ac340e6092cfcdc584a18e3f3f87df40da0adc
Kyle Malone <Kylejaxonmalone93@gmail.com>
2023-06-06 14:13:14 -0500

    attempting to fix frontend

--- PlayFlix-firebase-FrontEnd-main/src/components/games/GamesContainer.js
+++ PlayFlix-firebase-FrontEnd-main/src/components/games/GamesContainer.js
@@ -1,15 +1,19 @@
-
-import { GamesSearch } from "./GamesSearch"
-import { GamesList } from "./GamesList"
+import { GamesSearch } from "./GamesSearch";
+import { GamesList } from "./GamesList";
 import { FiArrowRightCircle } from "react-icons/fi";
 
-export const GamesContainer = ({searchParams, modalOpen, setModalOpen, returnedGames, setReturnedGames}) => {
-    return  <>
-        
-        <GamesList />
-        
-        <GamesSearch searchParams={searchParams} modalOpen={modalOpen} returnedGames={returnedGames} setReturnedGames={setReturnedGames} setModalOpen={setModalOpen} />
-        
+export const GamesContainer = ({ searchParams, modalOpen, setModalOpen, returnedGames, setReturnedGames }) => {
+  return (
+    <>
+      <GamesList />
+
+      <GamesSearch
+        searchParams={searchParams}
+        modalOpen={modalOpen}
+        returnedGames={returnedGames}
+        setReturnedGames={setReturnedGames}
+        setModalOpen={setModalOpen}
+      />
     </>
   );
 };
